8a80c552fabad060e45cb745198f3cbcc0b8d48b
Sagar Bharadwaj <sagar.bharadwaj@yahoo.com>
2018-03-18 15:02:12 +0530

    First Commit

---
+++ app/src/main/java/sagarb/solveit/CameraScanner.java
@@ -0,0 +1,149 @@
+package sagarb.solveit;
+
+import android.app.ProgressDialog;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.hardware.Camera;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class CameraScanner extends AppCompatActivity{
+
+    private Camera mCamera;
+    private CameraPreview mcameraPreview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera_scanner);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        mCamera = getCameraInstance();
+        mcameraPreview = new CameraPreview(this, mCamera);
+
+        FrameLayout preview = (FrameLayout)findViewById(R.id.camera_preview);
+        preview.addView(mcameraPreview);
+    }
+
+    public Camera getCameraInstance(){
+        Camera c = null;
+        try {
+            c = Camera.open();
+        }
+        catch (Exception e){
+            Toast.makeText(this,"Camera not available",Toast.LENGTH_LONG).show();
+        }
+        return c;
+    }
+
+    public void takePicture(View v){
+        mCamera.takePicture(null, null, mPicture);
+    }
+
+    private Camera.PictureCallback mPicture = new Camera.PictureCallback() {
+
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+            Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
+            bitmap = rotateImage(bitmap, 90);
+            uploadBitmap(bitmap);
+        }
+    };
+
+    public static Bitmap rotateImage(Bitmap source, float angle) {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(angle);
+        return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(), matrix, true);
+    }
+
+    public byte[] getFileDataFromDrawable(Bitmap bitmap) {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
+        return byteArrayOutputStream.toByteArray();
+    }
+
+    private void uploadBitmap(final Bitmap bitmap) {
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Please Wait");
+        progressDialog.setMessage("Loading..");
+
+        progressDialog.show();
+
+        VolleyMultipartRequest volleyMultipartRequest = new VolleyMultipartRequest(Request.Method.POST, URLs.UPLOAD_URL,
+                new Response.Listener<NetworkResponse>() {
+                    @Override
+                    public void onResponse(NetworkResponse response) {
+                        try {
+                            JSONObject obj = new JSONObject(new String(response.data));
+                            Toast.makeText(getApplicationContext(), obj.getString("solution"), Toast.LENGTH_SHORT).show();
+                            Log.d("Resopnse","This is Response : "+obj.toString());
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                        progressDialog.dismiss();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+                        Log.d("Resopnse","This is Error Response : "+error.getMessage());
+                        progressDialog.dismiss();
+                    }
+                }) {
+
+            /*
+            * If you want to add more parameters with the image
+            * you can do it here
+            * here we have only one parameter with the image
+            * which is tags
+            * */
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                return params;
+            }
+
+            /*
+            * Here we are passing image by renaming it with a unique name
+            * */
+            @Override
+            protected Map<String, DataPart> getByteData() {
+                Map<String, DataPart> params = new HashMap<>();
+                long imagename = System.currentTimeMillis();
+                params.put(URLs.IMAGE_PARAM_NAME, new DataPart(imagename + ".png", getFileDataFromDrawable(bitmap)));
+                return params;
+            }
+        };
+
+        //adding the request to volley
+        DefaultRetryPolicy  retryPolicy = new DefaultRetryPolicy(0, -1, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT);
+        volleyMultipartRequest.setRetryPolicy(retryPolicy);
+        Volley.newRequestQueue(this).add(volleyMultipartRequest);
+    }
+}
---
+++ app/src/main/java/sagarb/solveit/URLs.java
@@ -0,0 +1,14 @@
+package sagarb.solveit;
+
+/**
+ * Created by Dell on 17-03-2018.
+ */
+
+public class URLs {
+    public static final String UPLOAD_URL = "http://192.168.43.137:3000/imgupload";
+    public static final String IMAGE_PARAM_NAME = "digitimage";
+
+
+    //public static final String UPLOAD_URL = "http://192.168.43.136:8000/";
+    //public static final String IMAGE_PARAM_NAME = "img";
+}
